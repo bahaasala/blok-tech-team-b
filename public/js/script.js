caf84f1cb96ac85e86bb69606cdf03b9eeefe726
marcanthonybakker <marc.bakker2@hva.nl>
2023-06-13 14:43:40 +0200

    SCSS & JS Prog. Enh. correct

--- public/js/script.js
+++ public/js/script.js
@@ -53,18 +53,103 @@ window.addEventListener("load", () => {
   }, 500);
 });
 
-// Show submit screen when form is filled
 
-const showFormSubmitButton = document.querySelector(".review section:nth-of-type(4) button");
-const hiddenFormSend = document.querySelector(".review section:nth-of-type(5)");
-const hideFormSend = document.querySelector(".review section:nth-of-type(5) button");
 
-showFormSubmitButton.addEventListener("click", () => {
-  hiddenFormSend.classList.remove("hidden");
-  console.log("show submit screen");
+
+
+
+
+
+
+
+
+
+
+
+
+// Code for geolocation API and reverse geocoding from Geoapify
+const apiKey = "15a0ff507ba54cdeaa2699add37a5999"; // API key van Geoapify
+const locationReviewElementt = document.getElementById("locationReview");
+const locationReviewButton = document.getElementById("locationReviewButton");
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    // Check if geolocation is available
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    locationReviewElementt.value = "Geolocation is not available for your browser.";
+  }
+};
+
+
+// Reverse geocoding from Geoapify
+const onSuccess = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const reverseGeocodingUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${apiKey}`; // URL for reverse geocoding
+
+  fetch(reverseGeocodingUrl) // Fetch data of reverse geocoding
+    .then((response) => response.json()) // Data transfer to json
+    .then((data) => {
+      // Data of reverse geocoding
+      const address = data.features[0].properties.formatted;
+      locationReviewElementt.value = address;
+    })
+    .catch((error) => {
+      // Error handling for reverse geocoding
+      console.log("Error getting reverse geocoding data:", error);
+      locationReviewElementt.value = "Could not find address, an error has occurred";
+    });
+};
+
+
+// Error handling for geolocation
+const onError = (error) => {
+  locationReviewElementt.value = "Error found while loading your location: " + error.message;
+};
+
+locationReviewButton.addEventListener("click", getLocation);
+
+
+// Show submit button when everything is focused once
+const submitButton = document.querySelector(".review div input");
+const descriptionInput = document.querySelector(".review section:nth-of-type(2) textarea");
+let descriptionOn = false;
+const locationInput = document.querySelector(".review section:nth-of-type(3) input");
+let locationOn = false;
+
+const checkSubmitButton = () => {
+  if (descriptionOn && locationOn) {
+    console.log("submit on");
+    submitButton.classList.remove("hidden");
+  } else {
+    console.log("submit off");
+    submitButton.classList.add("hidden");
+  }
+};
+
+descriptionInput.addEventListener("focus", () => {
+  console.log("description clicked");
+  descriptionOn = true;
+  descriptionInput.classList.add("clickedReview");
+  checkSubmitButton();
 });
 
-hideFormSend.addEventListener("click", () => {
-  hiddenFormSend.classList.add("hidden");
-  console.log("hide submit screen");
+locationInput.addEventListener("focus", () => {
+  console.log("location clicked");
+  locationOn = true;
+  locationInput.classList.add("clickedReview");
+  checkSubmitButton();
 });
+
+// Check the initial state of the submit button
+checkSubmitButton();
+
+
+// Hide current location warning message when javascript is enabled
+const jsCheck = 1;
+const currentLocationWaringMessage = document.querySelector(".review section:nth-of-type(3) p");
+
+if (jsCheck == 1) {
+  currentLocationWaringMessage.classList.add("hidden");
+}
