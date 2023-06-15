bc387b59e0152a61c32f8269cfb232550ad76690
bahaasala <salaymeh1999@live.com>
2023-06-15 14:06:50 +0200

    test user in my bookings

--- controllers/userBookingsController.js
+++ controllers/userBookingsController.js
@@ -1,5 +1,10 @@
+const { db } = require("../connect")
+const User = require("../schemas/User")
+
 const userBookingsController = async (req, res, next) => {
   try {
+    const user = await User.findOne({ first_name: "Bahaa" })
+    // console.log(user)
     res.render("bookings.ejs", { title: "My bookings" })
   } catch (err) {
     next(err)
