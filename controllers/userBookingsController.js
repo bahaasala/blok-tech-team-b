5c3b7d45192443aa7b6566585654aa50db799045
bahaasala <salaymeh1999@live.com>
2023-06-19 21:08:50 +0200

    show bookings

--- controllers/userBookingsController.js
+++ controllers/userBookingsController.js
@@ -1,39 +1,12 @@
 const User = require("../schemas/User")
 const Booking = require("../schemas/Booking")
+const Trip = require("../schemas/Trip")
 
 const userBookingsController = async (req, res, next) => {
   try {
-    const userId = "64897eacdc14bf160eb8f20a" // Replace with the actual user ID
-    const user = await User.findOne({ _id: userId })
-
-    // console.log(user)
-
-    const bookings = await Booking.find({ user: userId })
-
+    const user = await User.findOne({ username: req.session.username })
+    const bookings = await Booking.find({ user: user._id }).populate("trip")
     console.log(bookings)
-
-    // const newBooking = new Booking({
-    //   user: user._id,
-    //   date_range: {
-    //     start_date: "2021-01-01T00:00:00.000Z",
-
-    //     end_date: "2021-01-01T00:00:00.000Z"
-    //   },
-    //   room: {
-    //     type: "Single",
-    //     gender: "Male",
-    //     price: 200
-    //   },
-    //   price: 200,
-    //   destination: "Paris"
-    // })
-
-    // await newBooking.save()
-
-    // console.log(newBooking)
-
-    // const bookings = await Booking.find({ _id: { $in: user.bookedTrips } })
-    // console.log(bookings)
 
     res.render("bookings.ejs", {
       title: "My bookings",
