e29753e92b3194ded7418d5d8383ce450540892e
Koen Dekker <96053886+Kvdekker@users.noreply.github.com>
2023-06-20 13:11:19 +0200

    Fixed image compression

--- controllers/changeProfilePictureController.js
+++ controllers/changeProfilePictureController.js
@@ -2,36 +2,49 @@ const { db } = require("../schemas/connect")
 const User = require("../schemas/User")
 const multer = require("multer")
 const fs = require("fs")
+const sharp = require("sharp")
 
-// Configuratie voor Multer om de bestanden op te slaan
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images") // Pad naar de map waar je de afbeeldingen wilt opslaan
+    cb(null, "public/images")
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
-    cb(null, file.fieldname + "-" + uniqueSuffix) // Bestandsnaam genereren
+    cb(null, file.fieldname + "-" + uniqueSuffix)
   }
 })
 
-// Middleware voor het uploaden van het bestand
 const upload = multer({
   storage,
   limits: { fileSize: 10 * 1024 * 1024 }
 }).single("profilePicture")
 
+const compressImage = async (source, destination, quality) => {
+  await sharp(source)
+    .jpeg({ quality: quality })
+    .png({ quality: quality })
+    .jpeg({ quality: quality })
+    .toFile(destination)
+}
+const compressProfilePicture = async (source, destination) => {
+  await compressImage(source, destination, 80)
+}
+
 const changeProfilePictureController = async (req, res, next) => {
   try {
-    // Afbeelding uploaden
     upload(req, res, async (err) => {
       if (err) {
         throw new Error("Upload failed.")
       }
 
       const { username } = req.session
-      const imageUrl = req.file.filename // Bestandsnaam verkrijgen van Multer
+      const imageUrl = req.file.filename
 
-      // Verwijder de oude profielfoto als deze niet de standaardfoto is
+      const sourcePath = `public/images/${imageUrl}`
+      const destinationPath = `public/compressed/${imageUrl}`
+
+      await compressProfilePicture(sourcePath, destinationPath)
+
       const user = await User.findOne({ username })
       if (user.image_url !== "default-user.png") {
         const path = `public/images/${user.image_url}`
@@ -44,7 +57,6 @@ const changeProfilePictureController = async (req, res, next) => {
         })
       }
 
-      // Update de image_url van de gebruiker in de database
       await User.findOneAndUpdate({ username }, { image_url: imageUrl })
 
       res.redirect("/account")
