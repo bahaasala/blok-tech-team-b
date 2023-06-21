4e8ee4ca44ff48140d773d2fc124c2bfe014f281
Mr Bats <timvaningen63@gmail.com>
2023-06-21 21:36:23 +0200

    add wishlist icon to my wishlist

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const wishlistController = require("../controllers/wishlistController")
+const manageWishlistController = require("../controllers/manageWishlistController")
 
 router.get("/", wishlistController)
+router.post("/", manageWishlistController)
 
 module.exports = router
